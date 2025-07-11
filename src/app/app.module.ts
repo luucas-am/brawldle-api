c663cbcb84bed05a43ca5f42721f773820837a8c
Lucas Andrade <lucas.andrade7899@gmail.com>
2025-07-11 11:29:53 -0300

    feat: added games and brawlers modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './database/prisma.module';
+import { GamesModule } from './modules/games/games.module';
+import { BrawlersModule } from './modules/brawlers/brawlers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    BrawlersModule,
+    GamesModule,
+  ],
   providers: [],
 })
+
 export class AppModule {}
